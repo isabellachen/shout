7afe222163defecc7f1fe4d6d92c7e14cd36a28d
Isabella Chen <miss.izzyc@gmail.com>
2021-02-26 12:55:36 +0100

    refactor header and menu css

--- themes/shout_theme/header.php
+++ themes/shout_theme/header.php
@@ -28,22 +28,7 @@
 	<header id="masthead" class="site-header">
     <div class="site-branding_header">
       <div class="site-branding">
-        <?php
-        the_custom_logo();
-        if ( is_front_page() && is_home() ) :
-          ?>
-          <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-          <?php
-        else :
-          ?>
-          <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-          <?php
-        endif;
-        $shout_theme_description = get_bloginfo( 'description', 'display' );
-        if ( $shout_theme_description || is_customize_preview() ) :
-          ?>
-          <p class="site-description"><?php echo $shout_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-        <?php endif; ?>
+        <?php the_custom_logo(); ?>
       </div><!-- .site-branding -->
       <div class="desktop-secondary_menu">
         <?php
